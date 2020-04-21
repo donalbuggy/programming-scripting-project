f88fc58f66a64047ead0fa8f20a14545d0b0a2f5
donalbuggy <buggyjdonal@gmail.com>
2020-04-21 16:02:19 +0100

    modified analysis.py code to separate species by colour in scatter plots

--- analysis.py
+++ analysis.py
@@ -2,48 +2,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import csv
 
 df = pd.read_csv("iris.csv")
+print(df.head)
+print(df)
 
-plt.scatter(df["petal_length"], df["petal_width"])
-plt.title("Petal length vs Petal width")
-plt.xlabel("Petal length")
-plt.ylabel("Petal width")
+g = sns.scatterplot(x="petal_length", y="petal_width", hue="species", data=df)
+g.set(xscale="linear")
 plt.savefig("pl_pw.png")
 plt.show()
 
-plt.scatter(df["petal_length"], df["sepal_length"])
-plt.title("Petal length vs Sepal length")
-plt.xlabel("Petal length")
-plt.ylabel("Sepal length")
+g = sns.scatterplot(x="petal_length", y="sepal_length", hue="species", data=df)
+g.set(xscale="linear")
 plt.savefig("pl_sl.png")
 plt.show()
 
-plt.scatter(df["petal_length"], df["sepal_width"])
-plt.title("Petal length vs Sepal width")
-plt.xlabel("Petal length")
-plt.ylabel("Sepal width")
-plt.savefig("pl_sl.png")
+g = sns.scatterplot(x="petal_length", y="sepal_width", hue="species", data=df)
+g.set(xscale="linear")
+plt.savefig("pl_sw.png")
 plt.show()
 
-plt.scatter(df["sepal_length"], df["petal_width"])
-plt.title("Sepal length vs Petal width")
-plt.xlabel("Sepal length")
-plt.ylabel("Petal width")
+g = sns.scatterplot(x="sepal_length", y="petal_width", hue="species", data=df)
+g.set(xscale="linear")
 plt.savefig("sl_pw.png")
 plt.show()
 
-plt.scatter(df["sepal_length"], df["sepal_width"])
-plt.title("Sepal length vs Sepal width")
-plt.xlabel("Sepal length")
-plt.ylabel("Sepal width")
+g = sns.scatterplot(x="sepal_length", y="sepal_width", hue="species", data=df)
+g.set(xscale="linear")
 plt.savefig("sl_sw.png")
 plt.show()
 
-plt.scatter(df["petal_width"], df["sepal_width"])
-plt.title("Petal width vs Sepal width")
-plt.xlabel("Petal Width")
-plt.ylabel("Sepal width")
+g = sns.scatterplot(x="petal_width", y="sepal_width", hue="species", data=df)
+g.set(xscale="linear")
 plt.savefig("pw_sw.png")
 plt.show()
 
